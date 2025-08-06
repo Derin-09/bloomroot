f86018b1a45b9e59fef92efdb92cd33cdd747903
Derin <aderinsolasamashimi@gmail.com>
2025-08-06 22:34:50 +0100

    removed footer error in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from '../components/HeroSection/HeroSection'
 import AboutSection from '../components/AboutSection/AboutSection'
 import ContactForm from '../components/ContactForm/ContactForm'
-import Footer from '../components/Footer/Footer'
 import FeaturedProducts from '../components/FeaturedProducts/FeaturedProducts'
 
 export default function Home() {
